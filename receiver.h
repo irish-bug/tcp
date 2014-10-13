1c6375d3b1bda2dfb66470d5ffb3abdf593b56e6
Shane <shaner@debian>
2014-10-12 21:30:15 -0500

    receiver files added

---
+++ receiver.h
@@ -0,0 +1,22 @@
+#ifndef RECEIVER_H
+#define RECEIVER_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MYPORT "4950"    // the port users will be connecting to
+
+#define MAXBUFLEN 100
+using namespace std;
+
+
+#endif
