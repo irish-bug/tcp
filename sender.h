7ff24f90017a18d6aa626e75cbd6b9e54ec9709e
Karen Lamb <lamb10@illinois.edu>
2014-10-13 13:15:23 -0500

    updating to pull a new copy to laptop

--- sender.h
+++ sender.h
@@ -3,6 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <iostream>
 #include <fstream>
 #include <string>
