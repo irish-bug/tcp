89f9adf42b893f0d66a8e3d845e69ed0608ae18b
Karen Lamb <lamb10@illinois.edu>
2014-10-13 13:15:32 -0500

    updating to pull a new copy to laptop

--- sender_main.cpp
+++ sender_main.cpp
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <ctime>
 #include "sender.h"
 
