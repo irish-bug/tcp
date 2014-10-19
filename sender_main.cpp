93594c61817cf84a61f515b03e35a157cdcef9d9
Shane <shaner@debian>
2014-10-19 12:18:33 -0500

    closing everything

--- sender_main.cpp
+++ sender_main.cpp
@@ -338,5 +338,6 @@ void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* file
 	receiver = thread(receiveACKs);
 
 	sender.join();
-    receiver.join(); 
+    receiver.join();
+    return; 
 }
